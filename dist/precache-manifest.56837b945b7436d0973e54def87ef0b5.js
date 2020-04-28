2b9d86bed2f7aa3d357c0188444aeeec2ca29c96
Santhosh Kumar <santhosh@anattadesign.com>
2020-04-28 23:07:38 +0530

    Initial dist subtree commit

---
+++ dist/precache-manifest.56837b945b7436d0973e54def87ef0b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "2c03adde3afae44d7204",
+    url: "/ScreenIndiaV2/css/app.c58f468e.css"
+  },
+  {
+    revision: "ee1507b63aebcd6ea80306ef4d894ffa",
+    url: "/ScreenIndiaV2/img/hero.ee1507b6.png"
+  },
+  {
+    revision: "99ccce59dbac1f48dd5d0963c85347d1",
+    url: "/ScreenIndiaV2/img/logo.99ccce59.png"
+  },
+  {
+    revision: "7fd135d89a6439e47eca502def5b7ce7",
+    url: "/ScreenIndiaV2/img/welcome.7fd135d8.jpg"
+  },
+  {
+    revision: "168ea0d9c33ff78ec413217166360396",
+    url: "/ScreenIndiaV2/index.html"
+  },
+  {
+    revision: "2c03adde3afae44d7204",
+    url: "/ScreenIndiaV2/js/app.ec3ecf4d.js"
+  },
+  {
+    revision: "1bb5a2b03647ffd5f131",
+    url: "/ScreenIndiaV2/js/chunk-vendors.902159f6.js"
+  },
+  {
+    revision: "898b2c1dfc6ef8289d66e2fbc1ff348f",
+    url: "/ScreenIndiaV2/manifest.json"
+  },
+  {
+    revision: "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    url: "/ScreenIndiaV2/robots.txt"
+  }
+]);
